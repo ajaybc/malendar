22157da086302c70d6bbaff567c9e2467ec0245c
Ajay Balachandran <ajayxiia@gmail.com>
2014-10-19 15:45:27 +0530

    Added flipping animation

--- source/scripts/app/controllers.js
+++ source/scripts/app/controllers.js
@@ -3,10 +3,23 @@ angular.module('Malendar.controllers', [])
 		todayMoment = moment();
 		todayString = todayMoment.format("D/M/YYYY");
 		dayDetails = Malendar.dates[todayString];
+		$scope.flipped = false;
 		$scope.gregorianMonth = monthProvider.getGregorianMonthName(todayMoment.month());
 		$scope.gregorianDate = todayMoment.date();
 		$scope.gregorianWeekDay = dayProvider.getGregorianWeekDayName(todayMoment.isoWeekday() - 1);
 		$scope.malayalamDate = (dayDetails.MDay < 10)?('0' + dayDetails.MDay):dayDetails.MDay;
 		$scope.malayalamMonth = monthProvider.getMalayalamMonthName(dayDetails.MalayalamMonth);
 		$scope.malayalamNakshatra = dayDetails.MNakshatra;
+
+		$scope.flip = function ($event) {
+			if ($scope.flipped) {
+				$scope.flipped = false;
+			} else {
+				$scope.flipped = true;
+			}
+
+			if ($event) {
+				$event.preventDefault();
+			}
+		}
 	}]);
